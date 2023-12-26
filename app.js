f6d8350ef926bbef8203338882e721b0e83f6a9d
Ankursanadhya <Ankur.Sanadhya@habilelabs.io>
2023-12-26 17:06:26 +0530

    AuthenticateUsingGoogle

---
+++ app.js
@@ -0,0 +1,76 @@
+const express = require("express");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+
+const app = express();
+
+// Set up sessions
+app.use(
+  require("express-session")({
+    secret: "your-secret-key",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+app.set("view engine", "ejs");
+
+// Initialize Passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Set up Google OAuth 2.0 authentication
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID:
+        "475413079775-111cm6rssldo5839043cl365j6kik922.apps.googleusercontent.com",
+      clientSecret: "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",
+      callbackURL: "http://localhost:3000/auth/google/callback",
+    },
+    (accessToken, refreshToken, profile, done) => {
+      return done(null, profile);
+    }
+  )
+);
+
+// Serialize user information to store in the session
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+// Deserialize user information from the session
+passport.deserializeUser((obj, done) => {
+  done(null, obj);
+});
+
+// Define routes
+app.get("/", (req, res) => {
+  res.render("home");
+});
+app.get("/afterLogin", (req, res) => {
+  res.render("afterLogin");
+});
+
+// Google authentication route
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["https://www.googleapis.com/auth/plus.login"],
+  })
+);
+
+// Google authentication callback route
+app.get(
+  "/auth/google/callback",
+  passport.authenticate("google", { failureRedirect: "/" }),
+  (req, res) => {
+    res.redirect("/afterLogin");
+  }
+);
+
+
+// Start the server
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
